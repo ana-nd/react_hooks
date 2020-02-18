b947ba42212a3f9b68be7a57ddf3185783ad9a74
Anand Kumar <kumaranand12368@gmail.com>
2020-02-19 01:40:52 +0530

    appbar component modifications

--- src/Components/Stories/Components/Appbar/index.js
+++ src/Components/Stories/Components/Appbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Grid, Typography, InputBase, InputAdornment, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, InputBase, InputAdornment, IconButton, Tooltip } from '@material-ui/core';
 import AppLogo from '../../../../images/logo-hn-search.png';
 import { Search, PowerSettingsNew } from '@material-ui/icons';
 import { withRouter } from 'react-router';
@@ -19,6 +19,21 @@ const useStyles = makeStyles((theme) =>
             background: theme.palette.common.white,
             margin: theme.spacing(0.5),
             padding: theme.spacing(0.625),
+            marginRight: "30px",
+            width: "75%",
+            [theme.breakpoints.down('lg')]: {
+                width: "70%",
+            },
+            [theme.breakpoints.down('sm')]: {
+                width: "55%",
+            },
+            [theme.breakpoints.down('xs')]: {
+                width: "35%",
+            },
+            logoutIcon: {
+                color: theme.palette.primary.contrastText,
+                marginLeft: theme.spacing(1)
+            }
         },
         startAdornment: {
             marginRight: theme.spacing(1.5)
@@ -27,10 +42,6 @@ const useStyles = makeStyles((theme) =>
             display: "flex",
             padding: theme.spacing(1.5)
         },
-        settingsIcon: {
-            fontSize: theme.spacing(3.75),
-            marginRight: theme.spacing(1.5)
-        }
     }),
 );
 
@@ -41,32 +52,25 @@ const Appbar = ({history}) => {
     return (
             <AppBar position="static">
                 <Toolbar>
-                    <Grid container>
-                        <Grid item xs={1} className={classes.leftContainer}>
-                            <img src={AppLogo} alt="logo" className={classes.logo}/>
-                        </Grid>
-                        <Grid item xs={9} className={classes.centerContainer}>
-                        <InputBase
-                            fullWidth
-                            autoFocus
-                            className={classes.input}
-                            placeholder="Search stories by title, url or author"
-                            inputProps={{ 'aria-label': 'Search stories by title, url or author' }}
-                            startAdornment={<InputAdornment className={classes.startAdornment}><Search color="primary"/></InputAdornment>}
-                        />
-                        </Grid>
-                        <Grid item xs={2} className={classes.rightContainer}>
-                            <Typography >
-                                by 
-                                <i className="fab fa-algolia" style={{fontSize: "25px", color: "#fff",verticalAlign :"bottom", marginLeft: "10px",marginRight: "10px"}}></i>
-                                <font style={{color: "#fff"}}>algolia</font>
-                                <i className="fas fa-sliders-h" style={{fontSize: "25px", color: "#fff",verticalAlign :"bottom", marginLeft: "20px"}}></i>
-                            </Typography>
-                            <IconButton onClick={()=>{window.localStorage.clear(); history.push("/")}}>
-                                <PowerSettingsNew />
-                            </IconButton>
-                        </Grid>
-                    </Grid>
+                    <img src={AppLogo} alt="logo" className={classes.logo}/>
+                    <InputBase
+                        autoFocus
+                        className={classes.input}
+                        placeholder="Search stories by title, url or author"
+                        inputProps={{ 'aria-label': 'Search stories by title, url or author' }}
+                        startAdornment={<InputAdornment className={classes.startAdornment}><Search color="primary"/></InputAdornment>}
+                    />
+                    <Typography>
+                        by 
+                        <i className="fab fa-algolia" style={{fontSize: "25px", color: "#fff",verticalAlign :"bottom", marginLeft: "10px",marginRight: "10px"}}></i>
+                        <font style={{color: "#fff"}}>algolia</font>
+                    </Typography>
+                    <i className="fas fa-sliders-h" style={{fontSize: "25px", color: "#fff",verticalAlign :"bottom", marginLeft: "20px",marginRight: "20px"}}></i>
+                    <Tooltip title="Logout" aria-label="logout">
+                        <IconButton onClick={()=>{window.localStorage.clear(); history.push("/")}} >
+                            <PowerSettingsNew />
+                        </IconButton>
+                    </Tooltip>
                 </Toolbar>
             </AppBar>
         )
